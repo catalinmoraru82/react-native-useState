6d9d1d0d6cb68bc046e18f7692709c05160eae80
Catalin Moraru <catalinmoraru@Catalins-MacBook-Air.local>
2023-02-02 23:08:18 +0200

    testing react useState

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+function App() {
+
+  const[count, setCount] = useState(0)
+
+  const [list, setList] = useState(['cata', 'bobi'])
+  const [name, setName] = useState('')
+
+  function increaseCount() {
+    setCount(prevState => {
+      return prevState + 1
+    })
+  }
+
+  function getName(e) {
+    setName(e.target.value)
+  }
+
+  function addName() {
+    setList([...list, name])
+    setName('')
+  }
+
+  return (
+    <>
+      <button onClick={increaseCount}>Count is: {count}</button>
+      <ul>
+        {
+          list.map(item => <li key={item}>{item}</li>)
+        }
+      </ul>
+      <input type='text' value={name} onChange={getName}/>
+      <button onClick={addName}>Add name</button>
+    </>
+  )
+}
+
+export default App
